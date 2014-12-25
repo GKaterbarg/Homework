b8bfdc2c9ba3ce58a5d36d6ffc41dee0d9f17d19
GKaterbarg <katerbarggleb@gmail.com>
2014-12-25 14:21:50 +0400

    Map Added

---
+++ AVLMap/AVLMap/Example.fs
@@ -0,0 +1,13 @@
+﻿module Example
+    
+    open MyMap
+    open System
+    open System.Diagnostics
+
+    let mutable myDictionary = MyMap.Map<_, _> [|(2, "b"); (3, "c"); (4, "d")|]
+    myDictionary <- myDictionary.Add 1 "a"
+    myDictionary <- myDictionary.Remove 4
+    printfn "%A" (myDictionary.Item(1))
+    for i in myDictionary do printfn "%A" i
+
+    Console.ReadLine() |> ignore
